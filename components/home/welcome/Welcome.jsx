de363ddcca252ee0d8997da3d25a7970268df3c3
Aryan Bansod <aryanem09@gmail.com>
2023-07-22 11:17:18 +0530

    minor

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -12,19 +12,52 @@ import { useRouter } from "expo-router";
 import styles from "./welcome.style";
 import { icons, SIZES } from "../../../constants";
 // import { useRouteNode } from "expo-router/build/Route";
+
+const jobTypes = ["Full Time", "Part Time", "Contractor"];
 const Welcome = () => {
   const router = useRouter();
+  const [activeJobTypes, setActiveJobTypes] = useState("Full-Time");
+
   return (
     <View>
       <View style={styles.container}>
         <Text style={styles.userName}>Ryn 09 </Text>
         <Text style={styles.welcomeMessage}>Find jobs made for you </Text>
       </View>
+
       <View style={styles.container}>
         <View style={styles.searchWrapper}>
-          <TextInput style={styles.searchInput} />
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="What kinda jobs are you looking for"
+          />
         </View>
-        {/* <Text style={styles.welcomeMessage}>Find jobs made for you </Text> */}
+        {/* <Text sty
+        le={styles.welcomeMessage}>Find jobs made for you </Text> */}
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtn}
+          ></Image>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={(item) => {
+            <TouchableOpacity
+              onPress={() => {
+                setActiveJobTypes(item);
+                router.push(`/search/${item}`);
+              }}
+            >
+              <Text style={styles.tab(activeJobTypes, item)}>{item}</Text>
+            </TouchableOpacity>;
+          }}
+        />
       </View>
     </View>
   );
